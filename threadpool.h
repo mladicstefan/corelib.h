8079cdf732806fddf1ae0b45c7f06e91760d21c0
mladicstefan <mladicstefan6@gmail.com>
2025-09-03 01:13:51 +0200

    threadpool_add

--- threadpool.h
+++ threadpool.h
@@ -4,10 +4,6 @@
 #define THREADPOOL_H
 #endif
 
-#ifdef __cplusplus
-extern "C"
-#endif
-
 #define MAX_THREADS 64
 #define MAX_QUEUE 65536
 
@@ -27,21 +23,23 @@ typedef struct {
 
 /*
  * Threadpool
- * lock - mutex for queue
+ * mutex - mutex for queue
  * threads - pointer to all pthread_t's made
  * queue - functions to be done
  *
  */
 typedef struct {
-    pthread_mutex_t lock;
+    pthread_mutex_t mutex;
     pthread_t *threads;
     pthread_cond_t notify;
     cpool_task_t *queue;
     int thread_count;
     int queue_size;
+    int pending_tasks;
     int started;
     int shutdown;
-
+    int head;
+    int tail;
 } threadpool_t;
 
 int threadpool_free(threadpool_t *pool);
@@ -72,7 +70,7 @@ threadpool_t *cpool_create(int thread_count, int queue_size)
         goto err;
     }
     // Initialize mutex and conditional (efficient callback for awaiting threads)
-    if ((pthread_mutex_init(&pool->lock, NULL) != 0) ||
+    if ((pthread_mutex_init(&pool->mutex, NULL) != 0) ||
         (pthread_cond_init(&(pool->notify), NULL) !=0)){
         goto err;
     }
@@ -81,7 +79,7 @@ threadpool_t *cpool_create(int thread_count, int queue_size)
         // todo: implement lol
         // if (pthread_create(&(pool->threads[i], NULL,threadpool_thread, void* pool)) )
         if (1){
-            // DONT FORGET TO FREE
+            // DONT FORGET TO FREE EXACTLY HREE!!! ON THIS LINE!!!!
             return NULL;
         }
         pool->thread_count++;
@@ -107,10 +105,48 @@ int threadpool_free(threadpool_t *pool)
         free(pool->threads);
         free(pool->queue);
 
-        pthread_mutex_lock(&(pool->lock));
-        pthread_mutex_destroy(&(pool->lock));
+        pthread_mutex_lock(&(pool->mutex));
+        pthread_mutex_destroy(&(pool->mutex));
         pthread_cond_destroy(&(pool->notify));
     }
     free(pool);
     return 0;
+}
+
+int threadpool_add(threadpool_t *pool,void (*function)(void *), void* argument)
+{
+    int next;
+
+    if (pool == NULL || function == NULL){
+       return -1;
+    }
+
+    if (pthread_mutex_lock(&(pool->mutex)) != 0){
+        return -2;
+    }
+
+    next = (pool->tail +1) % pool->queue_size;
+
+    if (pool->pending_tasks == pool->queue_size){
+        goto cleanup;
+    }
+
+    if (pool->shutdown){
+        goto cleanup;
+    }
+
+    pool->queue[pool->tail].function = function;
+    pool->queue[pool->tail].argument= argument;
+    pool->tail = next;
+    pool->pending_tasks++;
+
+    if(pthread_cond_signal(&(pool->notify)) != 0){
+        goto cleanup;
+    }
+
+    if(pthread_mutex_unlock(&(pool->mutex)) != 0){
+        goto cleanup;
+    }
+    cleanup:
+    //figure this out
 }
